105cf83465951e008aa6ebb93e1a4ac5e60c7896
Sade Paula <sadepaulaaquino@gmail.com>
2019-05-01 18:17:24 -0500

    everything works but click to blog page.

---
+++ main.py
@@ -0,0 +1,49 @@
+
+from flask import Flask, request, redirect, render_template
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+app.config['DEBUG'] = True
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://build-a-blog:createablog@localhost:8889/build-a-blog'
+app.config['SQLALCHEMY_ECHO'] = True
+db = SQLAlchemy(app)
+
+
+class Blog(db.Model):
+
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(120))
+    body = db.Column(db.String(120))
+
+    def __init__(self, title, body):
+        self.title = title
+        self.body = body
+        
+@app.route('/blog',methods =['GET','POST'])
+def blog():
+    blogs = Blog.query.all()
+    return render_template ("blog.html", blogs=blogs)
+    
+
+@app.route('/newblog', methods=['POST', 'GET'])
+def newpost():
+
+    if request.method == 'POST':
+        title = request.form['title']
+        body = request.form['body']
+        new_blog = Blog(title, body)
+        db.session.add(new_blog)
+        db.session.commit()
+        return redirect("/blog")
+    else:
+        print ("Error!")
+        
+
+    return render_template('newblog.html')
+
+@app.route ('/')
+def index():
+    return render_template('blog.html')
+
+if __name__ == '__main__':
+    app.run()
