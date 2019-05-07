6fe96d639c192c636712d196eb8725a17b0c8bda
Sade Paula <sadepaulaaquino@gmail.com>
2019-05-06 19:55:36 -0500

    works need to finish css

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 
-from flask import Flask, request, redirect, render_template
+from flask import Flask, request, redirect, render_template, url_for
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
@@ -7,8 +7,11 @@ app.config['DEBUG'] = True
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://build-a-blog:createablog@localhost:8889/build-a-blog'
 app.config['SQLALCHEMY_ECHO'] = True
 db = SQLAlchemy(app)
-
-
+app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 5
+"""def css_count():
+css_count = ""
+css_count += 1 
+return""" 
 class Blog(db.Model):
 
     id = db.Column(db.Integer, primary_key=True)
@@ -19,22 +22,30 @@ class Blog(db.Model):
         self.title = title
         self.body = body
         
-@app.route('/blog',methods =['GET','POST'])
-def blog():
-    blogs = Blog.query.all()
-    return render_template ("blog.html", blogs=blogs)
+
     
+@app.route('/blog',methods =['GET'])
+def blog():
+    if request.args:
+        blogpost_num = request.args.get("id")
+        blog = Blog.query.get(blogpost_num)
+        return render_template('individualblogs.html', blog=blog)
+
+    else:
+        blogs = Blog.query.all()
+        return render_template ("blog.html", blogs=blogs)
+
 
 @app.route('/newblog', methods=['POST', 'GET'])
 def newpost():
-
     if request.method == 'POST':
         title = request.form['title']
         body = request.form['body']
         new_blog = Blog(title, body)
         db.session.add(new_blog)
         db.session.commit()
-        return redirect("/blog")
+        blog_id = new_blog.id
+        return redirect ("/blog?id="+ str(blog_id))
     else:
         print ("Error!")
         
@@ -43,7 +54,7 @@ def newpost():
 
 @app.route ('/')
 def index():
-    return render_template('blog.html')
+    return redirect('/blog')
 
 if __name__ == '__main__':
     app.run()
