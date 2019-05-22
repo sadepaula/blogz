c0f6b397b5449573e1c0cd587ec1d7bd6a2acd72
Sade Paula <sadepaulaaquino@gmail.com>
2019-05-22 18:16:53 -0500

    missing the home page

--- main.py
+++ main.py
@@ -1,26 +1,88 @@
 
-from flask import Flask, request, redirect, render_template, url_for
+from flask import Flask, request, redirect, render_template, url_for, session, flash
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config['DEBUG'] = True
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://build-a-blog:createablog@localhost:8889/build-a-blog'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://blogz:createablog@localhost:8889/blogz'
 app.config['SQLALCHEMY_ECHO'] = True
 db = SQLAlchemy(app)
 app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 5
+app.secret_key = 'y337kGcys&zP3B'
 
 class Blog(db.Model):
 
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(120))
     body = db.Column(db.String(120))
+    owner_id= db.Column(db.Integer,db.ForeignKey('user.id'))
 
-    def __init__(self, title, body):
+    def __init__(self, title, body, owner):
         self.title = title
         self.body = body
-        
+        self.owner = owner
 
-    
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(120), unique=True)
+    password = db.Column(db.String(120))  
+    blogs = db.relationship('Blog', backref='owner') 
+
+    def __init__(self, username, password):
+        self.username = username
+        self.password = password
+        
+@app.before_request
+def require_login():
+    allowed_routes = ['login', 'signup','index']
+    if request.endpoint not in allowed_routes and 'username' not in session:
+        return redirect('/login')
+
+@app.route('/login', methods= ['POST', 'GET'])
+def login():
+    if request.method =='POST':
+        username = request.form['username']
+        password = request.form['password']
+        user = User.query.filter_by(username=username).first()
+        if user and user.password == password:
+            session['username'] = username
+            flash ("Logged In")
+
+            return redirect('/blog')
+        else:
+            flash('User password error or user does not exist', 'error')
+
+    return render_template('login.html')
+
+@app.route('/signup',methods= ['POST', 'GET']) 
+def signup():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        verify = request.form['verify']
+        
+        usernameError= ""
+        password1Error= ""
+        verifyError = ""
+
+        existing_user = User.query.filter_by(username=username).first()
+        if not existing_user:
+            new_user = User(username, password)
+            db.session.add(new_user)
+            db.session.commit()
+            session['username'] = username
+            return redirect('/blog')
+        else:
+            return "<h1> User Exists </h1>"
+
+
+    return render_template('signup.html')
+
+@app.route('/logout')
+def logout():
+    del session['username']
+    return redirect('/')
+
 @app.route('/blog',methods =['GET'])
 def blog():
     if request.args:
@@ -35,23 +97,32 @@ def blog():
 
 @app.route('/newblog', methods=['POST', 'GET'])
 def newpost():
+    owner = User.query.filter_by(username= session['username']).first()
+
     if request.method == 'POST':
         title = request.form['title']
         body = request.form['body']
-        new_blog = Blog(title, body)
+        new_blog = Blog(title, body, owner)
         db.session.add(new_blog)
         db.session.commit()
         blog_id = new_blog.id
         return redirect ("/blog?id="+ str(blog_id))
     else:
         print ("Error!")
-        
 
     return render_template('newblog.html')
 
-@app.route ('/')
+@app.route('/')
+def home():
+    return redirect ("/login")
+
+"""@app.route ('/index')
 def index():
-    return redirect('/blog')
+    users = User.query.all()
+    username= User.username  
+    print (users)  
+    return redirect("/blog?user=" + (username))"""
+    
 
 if __name__ == '__main__':
     app.run()
